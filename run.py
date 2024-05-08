f8f70b0fc7b42ca87f529555ef69cf5bf4020783
Seokgyeong Shin <jiku100@snu.ac.kr>
2024-05-08 11:30:13 +0900

    Fix saving format

--- run.py
+++ run.py
@@ -12,62 +12,56 @@ from configs import RESOLUTIONS
 
 def YoloTest(output_dir: str):
     models = ['yolov8n', 'yolov8s', 'yolov8m', 'yolov8l', 'yolov8x']
-    batch_sizes = [1]
-    # batch_sizes = [1, 2, 4, 8, 16]
-    
+    batch_size = 1
+
     benchmark_output_path = os.path.join(output_dir, "yolo.pkl")
 
     results = {}
 
+    resolutions = RESOLUTIONS
+    
+    results["resolutions"] = [resolution[1] for resolution in resolutions]
+
     for model in models:
         yolo = YoloLoader(model)
+        yolo.setBatchSize(batch_size)
 
-        result = {}
-
-        for resolution in RESOLUTIONS:
-            latencies = []
-
-            yolo.setResolution(resolution[0], resolution[1])
-
-            for batch_size in batch_sizes:
-                yolo.setBatchSize(batch_size)
-                latency = yolo.benchmark()
-                latencies.append(latency)
+        latencies = []
+        for resolution in resolutions:
             
-            result[resolution[1]] = latencies
-        
-        results[model] = result
+            yolo.setResolution(resolution[0], resolution[1])
+            latency = yolo.benchmark()
+            latencies.append(latency)
+      
+        results[model] = latencies
     
     with open(benchmark_output_path, "wb") as f:
         pickle.dump(results, f)
 
 def EfficientDetTest(output_dir: str):
     models = ['efficientdet-d0', 'efficientdet-d1', 'efficientdet-d2', 'efficientdet-d3', 'efficientdet-d4', 'efficientdet-d5', 'efficientdet-d6', 'efficientdet-d7']
-    batch_sizes = [1]
-    # batch_sizes = [1, 2, 4, 8, 16]
+    batch_size = 1
 
     benchmark_output_path = os.path.join(output_dir, "efficientdet.pkl")
 
     results = {}
 
+    resolutions = [RESOLUTIONS[0], RESOLUTIONS[2]]
+    
+    results["resolutions"] = [resolution[1] for resolution in resolutions]
+
     for model in models:
         efficientdet = EfficientDetLoader(model)
+        efficientdet.setBatchSize(batch_size)
 
-        result = {}
-
-        for resolution in [RESOLUTIONS[0], RESOLUTIONS[2]]:
-            latencies = []
-
-            efficientdet.setResolution(resolution[0], resolution[1])
-
-            for batch_size in batch_sizes:
-                efficientdet.setBatchSize(batch_size)
-                latency = efficientdet.benchmark()
-                latencies.append(latency)
-
-            result[resolution[1]] = latencies
+        latencies = []
+        for resolution in resolutions:
+            
+            efficientdet.setResolution(resolution[0], resolution[1])                
+            latency = efficientdet.benchmark()
+            latencies.append(latency)
         
-        results[model] = result
+        results[model] = latencies
 
     with open(benchmark_output_path, "wb") as f:
         pickle.dump(results, f)
@@ -79,23 +73,20 @@ def EfficientNetTest(output_dir: str):
     benchmark_output_path = os.path.join(output_dir, "efficientnet.pkl")
 
     results = {}
+    results['batch_sizes'] = batch_sizes
 
     for model in models:
         efficientnet = EfficientNetLoader(model)
-
-        result = {}
-        latencies = []
-
         efficientnet.setResolution(224, 224)
+
+        latencies = []
 
         for batch_size in batch_sizes:
             efficientnet.setBatchSize(batch_size)
             latency = efficientnet.benchmark()
             latencies.append(latency)
-
-        result[224] = latencies
         
-        results[model] = result
+        results[model] = latencies
 
     with open(benchmark_output_path, "wb") as f:
         pickle.dump(results, f)
@@ -105,24 +96,22 @@ def ResNetTest(output_dir: str):
     batch_sizes = [1, 2, 4, 8, 16]
 
     benchmark_output_path = os.path.join(output_dir, "resnet.pkl")
-    results = {}
 
+    results = {}
+    results['batch_sizes'] = batch_sizes
+    
     for model in models:
         resnet = ResNetLoader(model)
-
-        result = {}
-        latencies = []
-
         resnet.setResolution(224, 224)
+
+        latencies = []
 
         for batch_size in batch_sizes:
             resnet.setBatchSize(batch_size)
             latency = resnet.benchmark()
             latencies.append(latency)
 
-        result[224] = latencies
-
-        results[model] = result
+        results[model] = latencies
 
     with open(benchmark_output_path, "wb") as f:
         pickle.dump(results, f)
